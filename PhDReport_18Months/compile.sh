5ec3bd849329ccb1e677fbf4041cb56d8ec27a16
Joao Pela <joaopela@gmail.com>
2013-08-21 14:51:08 +0200

    Adding instructions to compile Feynman diagrams

---
+++ PhDReport_18Months/compile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mpost feynmanDiagram_GFHiggs.mp
+mpost feynmanDiagram_VBFHiggs.mp
+mpost feynmanDiagram_WZstrahlung.mp
+mpost feynmanDiagram_ttFHiggs.mp
+
+pdflatex JPela_PhDReport18M_Report.tex
+pdflatex JPela_PhDReport18M_Report.tex
