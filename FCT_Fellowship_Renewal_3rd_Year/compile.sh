048f0926bc13043b905a525dfb2902b0287a84d2
Joao Pela <joaopela@gmail.com>
2015-09-27 23:10:20 +0100

    Adding forgotten stutus

---
+++ FCT_Fellowship_Renewal_3rd_Year/compile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mpost feynmanDiagram_GFHiggs.mp
+mpost feynmanDiagram_VBFHiggs.mp
+mpost feynmanDiagram_WZstrahlung.mp
+mpost feynmanDiagram_ttFHiggs.mp
+
+pdflatex Renewal_3rd_Year_Report.tex
+pdflatex Renewal_3rd_Year_Report.tex
+
