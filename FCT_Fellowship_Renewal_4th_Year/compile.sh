34ab3beb60c19a0fd64cc7e524847a7e633b98c4
Joao Pela <joaopela@gmail.com>
2015-11-09 04:29:24 +0000

    Adding forgotten report

---
+++ FCT_Fellowship_Renewal_4th_Year/compile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mpost feynmanDiagram_GFHiggs.mp
+mpost feynmanDiagram_VBFHiggs.mp
+mpost feynmanDiagram_WZstrahlung.mp
+mpost feynmanDiagram_ttFHiggs.mp
+
+pdflatex Renewal_4th_Year_Report.tex
+pdflatex Renewal_4th_Year_Report.tex
+
